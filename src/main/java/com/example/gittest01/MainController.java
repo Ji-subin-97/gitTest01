becd9d2291729fee8e48924cfd3b824876764ba7
Ji_Subin <qls1178@naver.com>
2023-03-20 13:19:31 +0900

    커밋 테스트2

---
+++ src/main/java/com/example/gittest01/MainController.java
@@ -0,0 +1,17 @@
+package com.example.gittest01;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+@RequiredArgsConstructor
+public class MainController {
+
+    @GetMapping("/")
+    public String index(){
+        return "index";
+    }
+
+}
